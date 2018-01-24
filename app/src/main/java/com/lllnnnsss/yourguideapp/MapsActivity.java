edb69afae066fa0946cedfb8aa17f337e0f11207
elenes <kayagym@yahoo.com>
2018-01-24 18:28:39 +0300

    changed inicial location

--- app/src/main/java/com/lllnnnsss/yourguideapp/MapsActivity.java
+++ app/src/main/java/com/lllnnnsss/yourguideapp/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng SpbVO14_39 = new LatLng(59.939873, 30.269410);
+        mMap.addMarker(new MarkerOptions().position(SpbVO14_39).title("Marker in KarlMaySc_14_39"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SpbVO14_39));
     }
 }
