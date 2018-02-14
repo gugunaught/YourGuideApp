a8d8c0ed93d5c3b2c43f3ef10d757e5dcd2a061d
gugunaught <kayagym@yahoo.com>
2018-02-14 08:49:50 +0300

    Одна из библиотек по версии отличается от остальных. Студия ругается, но это не должно помешать работе. Стадия - 2 тестовых объекта. Всплывающее окно с текстом пока отсутствует.

--- app/src/main/java/com/lllnnnsss/yourguideapp/MapsActivity.java
+++ app/src/main/java/com/lllnnnsss/yourguideapp/MapsActivity.java
@@ -2,6 +2,7 @@ package com.lllnnnsss.yourguideapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.Manifest;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +11,39 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback //, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener
+  {
 
     private GoogleMap mMap;
+    //private int selectedPlaceId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +53,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -38,9 +71,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in 14 line 39 and move the camera at this point when the application is
         LatLng SpbVO14_39 = new LatLng(59.939873, 30.269410);
-        mMap.addMarker(new MarkerOptions().position(SpbVO14_39).title("Marker in KarlMaySc_14_39"));
+        mMap.addMarker(new MarkerOptions().position(SpbVO14_39).title("14 линия д. 39"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SpbVO14_39));
+        mMap.setMinZoomPreference(18);
+
+        LatLng SpbVO14_41 = new LatLng(59.940131, 30.268869);
+        mMap.addMarker(new MarkerOptions().position(SpbVO14_41).title("14 линия д. 41"));
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //   ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        //mMap.setOnMarkerClickListener(this);
+        //mMap.setOnInfoWindowClickListener(this);
     }
+
 }
